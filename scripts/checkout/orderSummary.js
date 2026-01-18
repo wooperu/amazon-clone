6325ce9867e659218d92a255ccc7b7886ab48e91
cloud <ckgbasilio@gmail.com>
2026-01-18 11:04:57 +0800

    save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -91,7 +91,7 @@ export function renderOrderSummary() {
 
       const priceString = deliveryOption.priceCents === 0
         ? 'FREE'
-        : `${formatCurrency(deliveryOption.priceCents)} -`;
+        : `$${formatCurrency(deliveryOption.priceCents)} -`;
 
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
